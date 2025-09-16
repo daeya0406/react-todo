5c740c5e26a3fdf9ffbe46456d8294af1312c7ef
daeya0406 <daeya0406@gmail.com>
2025-09-16 18:03:40 +0900

    무지개색 판별

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -6,14 +6,32 @@ import ResetButton from "./ResetButton";
 // 상태, input, onchange - 상태 조작, props, colorbox
 function App() {
   const [color, setColor] = useState("");
+
+  const handleChange = (e) => {
+    setColor(e.target.value);
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input value={color} onChange={(e) => setColor(e.target.value)} />
+        <input value={color} onChange={handleChange} />
       </div>
       <ResetButton onReset={() => setColor("")} />
       <ColorBox color={color} />
+      <p>
+        {![
+          "red",
+          "orange",
+          "yellow",
+          "green",
+          "blue",
+          "navy",
+          "purple",
+        ].includes(color)
+          ? "무지개색이 아닙니다."
+          : "무지개색이 맞습니다."}
+      </p>
     </div>
   );
 }
