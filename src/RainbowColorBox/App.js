467d3d0330615c80ef9d31a4b09b4ecec34f4d7d
최우석 <nick501@naver.com>
2025-09-16 17:22:57 +0900

    Input 값에 따른 색 변경 기능 구현

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,21 @@
-import ColorBox from "./ColorBox";
-
-import "./App.css";
+import ColorBox from './ColorBox';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('');
+
+  const handleColor = (e) => {
+    setColor(e.target.value);
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onChange={handleColor} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
